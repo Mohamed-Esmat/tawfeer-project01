1c3fb074564fdb15c493b81fae59680a927b8a35
Mohamed-Esmat-AbdElHafiz <msmt0452@gmail.com>
2022-07-02 08:11:56 +0200

    fianlly

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { HelmetProvider } from 'react-helmet-async';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StoreProvider } from './Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <StoreProvider>
+      <HelmetProvider>
+      <PayPalScriptProvider deferLoading={true}>
+          <App />
+        </PayPalScriptProvider>
+      </HelmetProvider>
+    </StoreProvider>
   </React.StrictMode>
 );
 
